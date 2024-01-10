444a1a2a9700a968c681f9eb5b4f383bcf747333
Massa Timothée <timothee.massa.perso@gmail.com>
2024-01-10 19:52:13 +0100

    transfered currenNoteId check from App return to Editor's value

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -77,14 +77,10 @@ export default function App() {
                     newNote={createNewNote}
                     deleteNote={deleteNote}
                 />
-                {
-                    currentNoteId && 
-                    notes.length > 0 &&
-                    <Editor 
-                        currentNote={currentNote} 
-                        updateNote={updateNote} 
-                    />
-                }
+                <Editor 
+                    currentNote={currentNote} 
+                    updateNote={updateNote} 
+                />
             </Split>
             :
             <div className="no-notes">
